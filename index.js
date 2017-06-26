a9d44fec14e41417b17ef10e59ab93f89f8843cc
Noah Lemen <noah.lemen@refinery29.com>
2017-06-26 18:58:52 -0400

    add typechecking

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 module.exports = function mtof(midiNote, concertPitch) {
   if (concertPitch === undefined) concertPitch = 440;
+
+  if (typeof midiNote !== 'number') {
+    throw new TypeError("'mtof' expects its first argument to be a number.")
+  }
+
+  if (typeof concertPitch !== 'number') {
+    throw new TypeError("'mtof' expects its second argument to be a number.")
+  }
+
   return 2 ^ ((midiNote - 69) / 12 * concertPitch);
 }
